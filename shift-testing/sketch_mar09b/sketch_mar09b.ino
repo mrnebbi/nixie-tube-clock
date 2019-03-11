ea8ccab096fa193530609b4c561c8356832a597c
Ian Nebbiolo <ian@ianisted.co.uk>
2019-03-11 13:00:20 +0000

    Update sketch_mar09b.ino

--- shift-testing/sketch_mar09b/sketch_mar09b.ino
+++ shift-testing/sketch_mar09b/sketch_mar09b.ino
@@ -38,66 +38,67 @@ sr.set(1, HIGH);
 sr.set(2, HIGH);
 sr.set(3, HIGH);
 }
-void TurnOnR1(){
-sr.set(0, LOW);
-sr.set(1, LOW);
-sr.set(2, LOW);
-sr.set(3, LOW);
-}
 void TurnOnR0(){
-sr.set(0, HIGH); 
+sr.set(0, LOW); 
 sr.set(1, LOW);
 sr.set(2, LOW);
 sr.set(3, LOW);
 }
-void TurnOnR9(){
+void TurnOnR1(){
+sr.set(0, HIGH);
+sr.set(1, LOW);
+sr.set(2, LOW);
+sr.set(3, LOW);
+}
+void TurnOnR2(){
 sr.set(0, LOW);
 sr.set(1, HIGH);
 sr.set(2, LOW);
 sr.set(3, LOW);
 }
-void TurnOnR8(){
+void TurnOnR3(){
 sr.set(0, HIGH);
 sr.set(1, HIGH);
 sr.set(2, LOW);
 sr.set(3, LOW);
 }
-void TurnOnR7(){
+void TurnOnR4(){
 sr.set(0, LOW);
-sr.set(1, LOW);
-sr.set(2, HIGH);
-sr.set(3, LOW);
-}
-void TurnOnR6(){
-sr.set(0, HIGH);
 sr.set(1, LOW);
 sr.set(2, HIGH);
 sr.set(3, LOW);
 }
 void TurnOnR5(){
+sr.set(0, HIGH);
+sr.set(1, LOW);
+sr.set(2, HIGH);
+sr.set(3, LOW);
+}
+void TurnOnR6(){
 sr.set(0, LOW);
 sr.set(1, HIGH);
 sr.set(2, HIGH);
 sr.set(3, LOW);
 }
-void TurnOnR4(){
+void TurnOnR7(){
 sr.set(0, HIGH);
 sr.set(1, HIGH);
 sr.set(2, HIGH);
 sr.set(3, LOW);
 }
-void TurnOnR3(){
+void TurnOnR8(){
 sr.set(0, LOW);
 sr.set(1, LOW);
 sr.set(2, LOW);
 sr.set(3, HIGH);
 }
-void TurnOnR2(){
+void TurnOnR9(){
 sr.set(0, HIGH);
 sr.set(1, LOW);
 sr.set(2, LOW);
 sr.set(3, HIGH);
 }
+
 
 ///////////////////////R////////////////////////////
 void TurnOffL(){
@@ -106,66 +107,76 @@ sr.set(5, HIGH);
 sr.set(6, HIGH);
 sr.set(7, HIGH);
 }
-void TurnOnL1(){
-sr.set(4, LOW);
-sr.set(5, LOW);
-sr.set(6, LOW);
-sr.set(7, LOW);
-}
 void TurnOnL0(){
-sr.set(4, HIGH); 
+sr.set(4, LOW); 
 sr.set(5, LOW);
 sr.set(6, LOW);
 sr.set(7, LOW);
 }
-void TurnOnL9(){
+void TurnOnL1(){
+sr.set(4, HIGH);
+sr.set(5, LOW);
+sr.set(6, LOW);
+sr.set(7, LOW);
+}
+void TurnOnL2(){
 sr.set(4, LOW);
 sr.set(5, HIGH);
 sr.set(6, LOW);
 sr.set(7, LOW);
 }
-void TurnOnL8(){
+void TurnOnL3(){
 sr.set(4, HIGH);
 sr.set(5, HIGH);
 sr.set(6, LOW);
 sr.set(7, LOW);
 }
-void TurnOnL7(){
+void TurnOnL4(){
 sr.set(4, LOW);
-sr.set(5, LOW);
-sr.set(6, HIGH);
-sr.set(7, LOW);
-}
-void TurnOnL6(){
-sr.set(4, HIGH);
 sr.set(5, LOW);
 sr.set(6, HIGH);
 sr.set(7, LOW);
 }
 void TurnOnL5(){
+sr.set(4, HIGH);
+sr.set(5, LOW);
+sr.set(6, HIGH);
+sr.set(7, LOW);
+}
+void TurnOnL6(){
 sr.set(4, LOW);
 sr.set(5, HIGH);
 sr.set(6, HIGH);
 sr.set(7, LOW);
 }
-void TurnOnL4(){
+void TurnOnL7(){
 sr.set(4, HIGH);
 sr.set(5, HIGH);
 sr.set(6, HIGH);
 sr.set(7, LOW);
 }
-void TurnOnL3(){
+void TurnOnL8(){
 sr.set(4, LOW);
 sr.set(5, LOW);
 sr.set(6, LOW);
 sr.set(7, HIGH);
 }
-void TurnOnL2(){
+void TurnOnL9(){
 sr.set(4, HIGH);
 sr.set(5, LOW);
 sr.set(6, LOW);
 sr.set(7, HIGH);
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -178,66 +189,67 @@ srM.set(1, HIGH);
 srM.set(2, HIGH);
 srM.set(3, HIGH);
 }
-void TurnOnMR1(){
-srM.set(0, LOW);
-srM.set(1, LOW);
-srM.set(2, LOW);
-srM.set(3, LOW);
-}
 void TurnOnMR0(){
-srM.set(0, HIGH); 
+srM.set(0, LOW); 
 srM.set(1, LOW);
 srM.set(2, LOW);
 srM.set(3, LOW);
 }
-void TurnOnMR9(){
+void TurnOnMR1(){
+srM.set(0, HIGH);
+srM.set(1, LOW);
+srM.set(2, LOW);
+srM.set(3, LOW);
+}
+void TurnOnMR2(){
 srM.set(0, LOW);
 srM.set(1, HIGH);
 srM.set(2, LOW);
 srM.set(3, LOW);
 }
-void TurnOnMR8(){
+void TurnOnMR3(){
 srM.set(0, HIGH);
 srM.set(1, HIGH);
 srM.set(2, LOW);
 srM.set(3, LOW);
 }
-void TurnOnMR7(){
+void TurnOnMR4(){
 srM.set(0, LOW);
-srM.set(1, LOW);
-srM.set(2, HIGH);
-srM.set(3, LOW);
-}
-void TurnOnMR6(){
-srM.set(0, HIGH);
 srM.set(1, LOW);
 srM.set(2, HIGH);
 srM.set(3, LOW);
 }
 void TurnOnMR5(){
+srM.set(0, HIGH);
+srM.set(1, LOW);
+srM.set(2, HIGH);
+srM.set(3, LOW);
+}
+void TurnOnMR6(){
 srM.set(0, LOW);
 srM.set(1, HIGH);
 srM.set(2, HIGH);
 srM.set(3, LOW);
 }
-void TurnOnMR4(){
+void TurnOnMR7(){
 srM.set(0, HIGH);
 srM.set(1, HIGH);
 srM.set(2, HIGH);
 srM.set(3, LOW);
 }
-void TurnOnMR3(){
+void TurnOnMR8(){
 srM.set(0, LOW);
 srM.set(1, LOW);
 srM.set(2, LOW);
 srM.set(3, HIGH);
 }
-void TurnOnMR2(){
+void TurnOnMR9(){
 srM.set(0, HIGH);
 srM.set(1, LOW);
 srM.set(2, LOW);
 srM.set(3, HIGH);
 }
+
 
 ///////////////////////R////////////////////////////
 void TurnOffML(){
@@ -246,63 +258,65 @@ srM.set(5, HIGH);
 srM.set(6, HIGH);
 srM.set(7, HIGH);
 }
-void TurnOnML1(){
-srM.set(4, LOW);
-srM.set(5, LOW);
-srM.set(6, LOW);
-srM.set(7, LOW);
-}
 void TurnOnML0(){
-srM.set(4, HIGH); 
+srM.set(4, LOW); 
 srM.set(5, LOW);
 srM.set(6, LOW);
 srM.set(7, LOW);
 }
-void TurnOnML9(){
+void TurnOnML1(){
+srM.set(4, HIGH);
+srM.set(5, LOW);
+srM.set(6, LOW);
+srM.set(7, LOW);
+}
+void TurnOnML2(){
 srM.set(4, LOW);
 srM.set(5, HIGH);
 srM.set(6, LOW);
 srM.set(7, LOW);
 }
-void TurnOnML8(){
+void TurnOnML3(){
 srM.set(4, HIGH);
 srM.set(5, HIGH);
 srM.set(6, LOW);
 srM.set(7, LOW);
 }
-void TurnOnML7(){
+void TurnOnML4(){
 srM.set(4, LOW);
-srM.set(5, LOW);
-srM.set(6, HIGH);
-srM.set(7, LOW);
-}
-void TurnOnML6(){
-srM.set(4, HIGH);
 srM.set(5, LOW);
 srM.set(6, HIGH);
 srM.set(7, LOW);
 }
 void TurnOnML5(){
+srM.set(4, HIGH);
+srM.set(5, LOW);
+srM.set(6, HIGH);
+srM.set(7, LOW);
+}
+void TurnOnML6(){
 srM.set(4, LOW);
 srM.set(5, HIGH);
 srM.set(6, HIGH);
 srM.set(7, LOW);
 }
-void TurnOnML4(){
+void TurnOnML7(){
 srM.set(4, HIGH);
 srM.set(5, HIGH);
 srM.set(6, HIGH);
 srM.set(7, LOW);
 }
-void TurnOnML3(){
+void TurnOnML8(){
 srM.set(4, LOW);
 srM.set(5, LOW);
 srM.set(6, LOW);
 srM.set(7, HIGH);
 }
-void TurnOnML2(){
+void TurnOnML9(){
 srM.set(4, HIGH);
 srM.set(5, LOW);
 srM.set(6, LOW);
 srM.set(7, HIGH);
 }
+
+
