457ba6b1794b132f5656de4f5ab8f6348d274809
Ian Nebbiolo <ian@ianisted.co.uk>
2019-04-01 00:41:03 +0100

    Added interrupt test
    - Added interrupt test script
    - Added breaking apart of digits to showTime()

--- Nixie/Nixie.ino
+++ Nixie/Nixie.ino
@@ -1,46 +1,175 @@
 /*
-  ShiftRegister74HC595.h - Library for easy control of the 74HC595 shift register.
-  Created by Timo Denk (www.simsso.de), Nov 2014.
-  Additional information are available on http://shiftregister.simsso.de/
-  Released into the public domain.
-*/
+ * Nixie tube setup
+ */
 
-#include <ShiftRegister74HC595.h>
+  #include <ShiftRegister74HC595.h>
+  
+  // create shift register object (number of shift registers, data pin, clock pin, latch pin)
+  ShiftRegister74HC595 ShiftHrs (1, 4, 5, 6); 
+  ShiftRegister74HC595 ShiftMins (1, 6, 7, 8);
+  
+  int delayTime = 1000;
+  int shortDelayTime = 150;
+  int pinSet = 0;
+  
+  boolean digits[10][4] = {
+    { LOW, LOW, LOW, LOW }, // ZERO
+    { HIGH, LOW, LOW, LOW}, // ONE
+    { LOW, HIGH, LOW, LOW }, // TWO
+    { HIGH, HIGH, LOW, LOW }, // THREE
+    { LOW, LOW, HIGH, LOW }, // FOUR
+    { HIGH, LOW, HIGH, LOW }, // FIVE
+    { LOW, HIGH, HIGH, LOW }, // SIX
+    { HIGH, HIGH, HIGH, LOW }, // SEVEN
+    { LOW, LOW, LOW, HIGH }, // EIGHT
+    { HIGH, LOW, LOW, HIGH } // NINE
+  };
+  
+  int pins[2][4] = {
+    { 0, 1, 2, 3 },
+    { 4, 5, 6, 7 }
+  };
 
-// create shift register object (number of shift registers, data pin, clock pin, latch pin)
-ShiftRegister74HC595 ShiftHrs (1, 2, 3, 4); 
-ShiftRegister74HC595 ShiftMins (1, 5, 6, 7);
 
-int delayTime = 1000;
-int shortDelayTime = 150;
-int pinSet = 0;
+/*
+ * Date/time & button setup
+ */
+  
+  const int setButton = 2;
+  const int addButton = 3;
+  
+  int mode = 0;
+  int countHr = 0;
+  int countMin = 0;
+  
+  bool blinkState = false;
+  unsigned long time_now = 0; // Interrupt timer for debouncing
 
-boolean digits[10][4] = {
-  { LOW, LOW, LOW, LOW }, // ZERO
-  { HIGH, LOW, LOW, LOW}, // ONE
-  { LOW, HIGH, LOW, LOW }, // TWO
-  { HIGH, HIGH, LOW, LOW }, // THREE
-  { LOW, LOW, HIGH, LOW }, // FOUR
-  { HIGH, LOW, HIGH, LOW }, // FIVE
-  { LOW, HIGH, HIGH, LOW }, // SIX
-  { HIGH, HIGH, HIGH, LOW }, // SEVEN
-  { LOW, LOW, LOW, HIGH }, // EIGHT
-  { HIGH, LOW, LOW, HIGH } // NINE
-};
 
-int pins[2][4] = {
-  { 0, 1, 2, 3 },
-  { 4, 5, 6, 7 }
-};
+/*
+ * Setup function
+ */
 
-void setup() { 
+void setup() {
+  
+  // put your setup code here, to run once:
+  pinMode(setButton, INPUT_PULLUP);
+  pinMode(addButton, INPUT_PULLUP);
+
+
+  Serial.begin(115200);
+  Serial.println("Start");
+  
+  attachInterrupt(digitalPinToInterrupt(setButton),setButtonPress,FALLING);
+  attachInterrupt(digitalPinToInterrupt(addButton),addButtonPress,FALLING);
+
 }
 
 void loop() {
-  CycleDigits();
+  if (mode == 0) {
+    showTime();
+  }
+  delay(500);
 }
 
 
+void showTime() {
+    if (countHr < 10) {
+      Serial.print("0");
+    }
+    Serial.print(countHr);
+    Serial.print(":");
+    if (countMin < 10) {
+      Serial.print("0");
+    }
+    Serial.print(countMin);
+
+    int hr1 = (countHr / 10) % 10;
+    int hr2 = countHr % 10;
+
+    int min1 = (countMin / 10) % 10;
+    int min2 = countMin % 10;
+
+    Serial.print(" (A:");
+    Serial.print(hr1);
+    Serial.print(" B:");
+    Serial.print(hr2);
+    Serial.print(" C:");
+    Serial.print(min1);
+    Serial.print(" D:");
+    Serial.print(min2);
+    Serial.println(")");
+}
+
+void addHour(){
+
+  if (countHr >= 23) {
+    countHr = 0;
+  } else {
+    countHr = countHr + 1;
+  }
+  
+  showTime();
+}
+
+void addMin(){
+
+  if (countMin >= 59) {
+    countMin = 0;
+  } else {
+    countMin = countMin + 1;
+  }
+
+  showTime();
+}
+
+
+// Handle button presses and pass off to setter functions
+
+void setButtonPress(){
+//  Serial.println("Set");
+  if(millis() > time_now + 200){
+    time_now = millis();
+    
+    if (mode > 1) {
+      mode = 0;
+    } else {
+      mode = mode + 1;
+    }
+
+        
+    switch (mode) {
+      case 1:
+          // flash hours
+        break;
+      case 2:
+          // flash mins
+        break;
+      default:
+          // revert to time display
+        break;
+    }
+    
+  }
+}
+
+void addButtonPress(){
+//  Serial.println("Add");
+  if(millis() > time_now + 200){
+    time_now = millis();
+    if (mode == 1) {
+      addHour();
+    } else if (mode == 2) {
+      addMin();
+    }
+  }
+}
+
+
+
+/*
+ * Functions to drive Nixie Tubes
+ */
 
 /*
  * SetDigit(position, digit)
@@ -120,4 +249,5 @@ void CycleTubes() {
   ClearAll();
   delay(delayTime);
 }
+
 
